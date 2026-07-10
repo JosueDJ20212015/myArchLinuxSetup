7351e0e886a9c0d225965180d8994c91d8c21d11
Elmer Romero <josue20212015@gmail.com>
2026-07-09 20:32:47 -0600

    Test Push Script

--- dotFiles-push.sh
+++ dotFiles-push.sh
@@ -4,4 +4,4 @@ git add .
 echo "Ingresa el mensaje del commit:"
 read commit_message
 git commit -m "$commit_message"
-git push https://JosueDJ20212015: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@github.com/JosueDJ20212015/$(basename $(git config --get remote.origin.url) .git).git
+git push
